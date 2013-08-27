8b6ae7aa61d907cd3d7d6d7238205cbe736706ca
Liang <Liang@Liang-PC>
2013-08-27 23:15:21 +0800

    增加修改课程表的测试Activity

---
+++ Zcalendar/src/test/TestEditClass.java
@@ -0,0 +1,16 @@
+package test;
+
+import com.example.zcalendar.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestEditClass extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.editclass_layout);
+	}
+}
